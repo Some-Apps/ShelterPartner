b6b67f91f8d5eeab5284f1dc4201e941f275f1a2
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-06-07 16:46:25 -0500

    Filtering (#24)
    * filter
    
    * closer
    
    * connected to firebase
    
    * Update AdvancedSettingsView.swift

---
+++ Apple/PawPartner/Views/Main Views/AccountSetupView.swift
@@ -0,0 +1,45 @@
+import AlertToast
+import SwiftUI
+
+struct AccountSetupView: View {
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var viewModel = SettingsViewModel.shared
+
+    @ObservedObject var authViewModel = AuthenticationViewModel.shared
+    
+    @State private var showingPasswordPrompt = false
+    @State private var passwordInput = ""
+    @State private var showIncorrectPassword = false
+    
+    @State private var shelterName = ""
+    @State private var managementSoftware = ""
+    
+    let managementSoftwareOptions = ["", "ShelterLuv", "ShelterManager"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Shelter Name", text: $viewModel.shelter)
+                    Picker("Management Software", selection: $viewModel.software) {
+                        ForEach(managementSoftwareOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Section {
+                    TextField("API Key", text: $viewModel.apiKey)
+                    NavigationLink("Can Play Filter", destination: FilterView())
+                }
+                Section {
+                    Button("Save") {
+                        viewModel.updateAccountSettings(shelter: viewModel.shelter, software: viewModel.software, apiKey: viewModel.apiKey, mainFilter: viewModel.mainFilter)
+                        dismiss()
+                        viewModel.showAccountUpdated = true
+                    }
+                }
+            }
+        }
+
+    }
+}
--- Apple/PawPartner/Views/Main Views/AdvancedSettingsView.swift
+++ Apple/PawPartner/Views/Main Views/AdvancedSettingsView.swift
@@ -252,3 +252,5 @@ struct AdvancedSettingsView: View {
         }
     }
 }
+
+
