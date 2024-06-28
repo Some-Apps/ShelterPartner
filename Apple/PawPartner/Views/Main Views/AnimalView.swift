0998e1e05a1cf381a00a3203747fffd33074ba05
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-06-28 15:39:38 -0500

    fixed new sstoast

--- Apple/PawPartner/Views/Main Views/AnimalView.swift
+++ Apple/PawPartner/Views/Main Views/AnimalView.swift
@@ -5,7 +5,7 @@ import AlertToast
 import Kingfisher
 import WebKit
 import UIKit
-import SSToastMessage
+//import SSToastMessage
 
 struct AnimalView: View {
     // MARK: - Properties
@@ -281,7 +281,7 @@ struct AnimalView: View {
                 viewModel.postAppVersion(societyID: storedSocietyID, installedVersion: "\(appVersion) (\(buildNumber))")
             }
         }
-        .present(isPresented: $shouldPresentThankYouView, type: .alert, autohideDuration: 60, closeOnTap: false) {
+        .present(isPresented: $shouldPresentThankYouView, type: .alert, duration: 60, closeOnTap: false) {
             ThankYouView(animal: viewModel.animal)
         }
         .toast(isPresenting: $showIncorrectPassword) {
@@ -306,11 +306,10 @@ struct AnimalView: View {
                 .scaledToFit()
                 .frame(maxWidth: 500)
         }
-        .present
-        .present(isPresented: $viewModel.showRequireName, type: .alert, autohideDuration: 60, closeOnTap: false, closeOnTapOutside: false) {
+        .present(isPresented: $viewModel.showRequireName, type: .alert, duration: 60, closeOnTap: false, closeOnTapOutside: false) {
             RequireNameView(animal: viewModel.animal)
         }
-        .present(isPresented: $viewModel.showRequireReason, type: .alert, autohideDuration: 60, closeOnTap: false, closeOnTapOutside: false) {
+        .present(isPresented: $viewModel.showRequireReason, type: .alert, duration: 60, closeOnTap: false, closeOnTapOutside: false) {
             RequireReasonView(animal: viewModel.animal)
         }
         .sheet(isPresented: $viewModel.showAddNote) {
