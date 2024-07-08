2dee0df3c05093ade3ec6fcec8a4841a14a68e58
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-07-08 11:23:28 -0500

    closer

--- Apple/PawPartner/Views/Main Views/AuthenticationView.swift
+++ Apple/PawPartner/Views/Main Views/AuthenticationView.swift
@@ -5,30 +5,52 @@ import FirebaseFirestore
 
 struct AuthenticationView: View {
     @ObservedObject var viewModel = AuthenticationViewModel.shared
+    @AppStorage("adminMode") var adminMode = true
+
     @State private var geoRestrict = false
 
     var body: some View {
         if viewModel.isSignedIn {
             switch viewModel.accountType {
             case "admin":
-                TabView {
-                    AnimalView()
-                        .tabItem {
-                            Label("Volunteer", systemImage: "pawprint.fill")
-                        }
-                    VisitorView()
-                        .tabItem {
-                            Label("Visitor", systemImage: "person.2.circle.fill")
-                        }
-                    SettingsView()
-                        .tabItem {
-                            Label("Settings", systemImage: "gearshape.fill")
-                        }
-                }
-                .onAppear {
-                    print("Admin")
-                    print(viewModel.shelterID)
-                    print(viewModel.accountType)
+                if !adminMode {
+                    TabView {
+                        AnimalView()
+                            .tabItem {
+                                Label("Volunteer", systemImage: "pawprint.fill")
+                            }
+                        VisitorView()
+                            .tabItem {
+                                Label("Visitor", systemImage: "person.2.circle.fill")
+                            }
+                    }
+                    .onAppear {
+                        print("Admin")
+                        print(viewModel.shelterID)
+                        print(viewModel.accountType)
+                    }
+
+                } else {
+                    TabView {
+                        AnimalView()
+                            .tabItem {
+                                Label("Volunteer", systemImage: "pawprint.fill")
+                            }
+                        VisitorView()
+                            .tabItem {
+                                Label("Visitor", systemImage: "person.2.circle.fill")
+                            }
+                        SettingsView()
+                            .tabItem {
+                                Label("Settings", systemImage: "gearshape.fill")
+                            }
+                    }
+                    .onAppear {
+                        print("Admin")
+                        print(viewModel.shelterID)
+                        print(viewModel.accountType)
+                    }
+
                 }
 
             case "volunteer":
@@ -50,7 +72,7 @@ struct AuthenticationView: View {
                             }
                     }
                     .onAppear {
-//                        checkGeorestriction()
+                        checkGeorestriction()
                         print("Volunteer")
                         print(viewModel.shelterID)
                         print(viewModel.accountType)
@@ -69,7 +91,7 @@ struct AuthenticationView: View {
                         }
                 }
                 .onAppear {
-//                    checkGeorestriction()
+                    checkGeorestriction()
                     print("Other")
                     print(viewModel.shelterID)
                     print(viewModel.accountType)
@@ -79,49 +101,50 @@ struct AuthenticationView: View {
             LoginView()
         }
     }
-//
-//    func checkGeorestriction() {
-//        let locationManager = CLLocationManager()
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.startUpdatingLocation()
-//        
-//        guard let currentLocation = locationManager.location else {
-//            // Handle the case where location is not available
-//            self.geoRestrict = true
-//            return
-//        }
-//        
-//        let db = Firestore.firestore()
-//        let shelterDocRef = db.collection("shelters").document(viewModel.shelterID)
-//        shelterDocRef.getDocument { document, error in
-//            if let document = document, document.exists, let data = document.data() {
-//                if let georestriction = data["georestriction"] as? [String: Any],
-//                   let enabled = georestriction["enabled"] as? Bool, enabled,
-//                   let center = georestriction["center"] as? GeoPoint,
-//                   let radius = georestriction["radius"] as? Double {
-//                    
-//                    let centerLocation = CLLocation(latitude: center.latitude, longitude: center.longitude)
-//                    let distance = currentLocation.distance(from: centerLocation)
-//                    
-//                    if distance > radius {
-//                        print("User outside of area")
-//                        if viewModel.accountType == "volunteer" {
-//                            self.geoRestrict = true
-//                        } else {
-//                            self.geoRestrict = false
-//                        }
-//                    } else {
-//                        print("User inside area")
-//                        self.geoRestrict = false
-//                    }
-//                }
-//            }
-//        }
-//    }
+
+    func checkGeorestriction() {
+        let locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        guard let currentLocation = locationManager.location else {
+            // Handle the case where location is not available
+            self.geoRestrict = true
+            return
+        }
+        
+        guard !viewModel.shelterID.isEmpty else {
+            return
+        }
+        
+        
+        
+        let db = Firestore.firestore()
+        let shelterDocRef = db.collection("Societies").document(viewModel.shelterID)
+        shelterDocRef.getDocument { document, error in
+            if let document = document, document.exists, let data = document.data() {
+                if let georestriction = data["georestriction"] as? [String: Any],
+                   let enabled = georestriction["enabled"] as? Bool, enabled,
+                   let center = georestriction["center"] as? GeoPoint,
+                   let radius = georestriction["radius"] as? Double {
+                    
+                    let centerLocation = CLLocation(latitude: center.latitude, longitude: center.longitude)
+                    let distance = currentLocation.distance(from: centerLocation)
+                    
+                    if distance > radius {
+                        print("User outside of area")
+                        if viewModel.accountType == "volunteer" {
+                            self.geoRestrict = true
+                        } else {
+                            self.geoRestrict = false
+                        }
+                    } else {
+                        print("User inside area")
+                        self.geoRestrict = false
+                    }
+                }
+            }
+        }
+    }
 }
 
-//struct GeoRestrictionView: View {
-//    var body: some View {
-//        Text("You are outside the allowed area to access this app.")
-//    }
-//}
