44450cbdd3e4242f62b0889dc4be5d4b423cee9e
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-07-10 15:52:57 -0500

    Update AuthenticationView.swift

--- Apple/PawPartner/Views/Main Views/AuthenticationView.swift
+++ Apple/PawPartner/Views/Main Views/AuthenticationView.swift
@@ -72,68 +72,6 @@ struct AuthenticationView: View {
                             }
                     }
                 }
-//                if let center = viewModel.locationSettings.center,
-//                   viewModel.locationSettings.enabled,
-//                   let radius = viewModel.locationSettings.radius,
-//                   !locationManager.isWithinBounds(center: CLLocationCoordinate2D(latitude: center.latitude, longitude: center.longitude), radius: radius) {
-//                    GeoRestrictionView()
-//                        .onAppear {
-//                            guard viewModel.locationSettings.enabled else {
-//                                       print("Location settings are disabled.")
-//                                       return
-//                                   }
-//
-//                                   guard let centerGeoPoint = viewModel.locationSettings.center else {
-//                                       print("Location center is nil.")
-//                                       return
-//                                   }
-//
-//                                   guard let radius = viewModel.locationSettings.radius else {
-//                                       print("Location radius is nil.")
-//                                       return
-//                                   }
-//
-//                                   // Log the GeoPoint values
-//                                   print("GeoPoint center: latitude \(centerGeoPoint.latitude), longitude \(centerGeoPoint.longitude)")
-//
-//                                   let center = CLLocationCoordinate2D(latitude: centerGeoPoint.latitude, longitude: centerGeoPoint.longitude)
-//
-//                                   // Log the converted CLLocationCoordinate2D values
-//                                   print("CLLocationCoordinate2D center: latitude \(center.latitude), longitude \(center.longitude)")
-//
-//                                   print("Location settings enabled: \(viewModel.locationSettings.enabled)")
-//                                   print("Location radius: \(radius)")
-//                                   print("Current location: \(String(describing: locationManager.location))")
-//
-//                                   if !locationManager.isWithinBounds(center: center, radius: radius) {
-//                                       // Your code logic here
-//                                       print("Location is out of bounds.")
-//                                   } else {
-//                                       print("Location is within bounds.")
-//                                   }
-//                        }
-//                } else {
-//                    TabView {
-//                        AnimalView()
-//                            .tabItem {
-//                                Label("Volunteer", systemImage: "pawprint.fill")
-//                            }
-//                        VisitorView()
-//                            .tabItem {
-//                                Label("Visitor", systemImage: "person.2.circle.fill")
-//                            }
-//                        VisitorSettingsView()
-//                            .tabItem {
-//                                Label("Settings", systemImage: "gearshape.fill")
-//                            }
-//                    }
-//                    .onAppear {
-//                        print("Volunteer")
-//                        print(viewModel.shelterID)
-//                        print(viewModel.accountType)
-//                    }
-//
-//                }
                 
             default:
                 TabView {
@@ -145,11 +83,10 @@ struct AuthenticationView: View {
                         .tabItem {
                             Label("Visitor", systemImage: "person.2.circle.fill")
                         }
-                }
-                .onAppear {
-                    print("Other")
-                    print(viewModel.shelterID)
-                    print(viewModel.accountType)
+                    VisitorSettingsView()
+                        .tabItem {
+                            Label("Settings", systemImage: "gearshape.fill")
+                        }
                 }
             }
         } else {
