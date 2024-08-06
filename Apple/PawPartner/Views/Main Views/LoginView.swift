d1daf426f40af1fd574ee8ae39379cf8eb409604
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-08-06 17:52:51 -0500

    added LoginViewModel

--- Apple/PawPartner/Views/Main Views/LoginView.swift
+++ Apple/PawPartner/Views/Main Views/LoginView.swift
@@ -1,42 +1,9 @@
-import AlertToast
-import FirebaseFirestore
-import FirebaseAuth
 import SwiftUI
-import SafariServices
-
-struct SafariView: UIViewControllerRepresentable {
-    let url: URL
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
-        return SFSafariViewController(url: url)
-    }
-
-    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
-    }
-}
+import AlertToast
 
 struct LoginView: View {
-    @State private var showNewShelterForm = false
-    @State private var showTutorials = false
-    @State private var email = ""
-    @State private var password = ""
-    @State private var showLoginError = false
-    @State private var loginError = ""
-    @State private var isLoginInProgress = false
-    @State private var newShelterForm = ""
-    @State private var tutorialsURL = ""
-
-    @AppStorage("lastSync") var lastSync: String = ""
-    private let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .short
-        return formatter
-    }()
-    @State private var showVideo = false
-
-    var loginListener: ListenerRegistration?
-
+    @StateObject var viewModel = LoginViewModel()
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -49,14 +16,13 @@ struct LoginView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: UIDevice.current.userInterfaceIdiom == .pad ? geometry.size.width * 0.3 : geometry.size.width * 0.9)
-                    
                         .onAppear {
-                            lastSync = dateFormatter.string(from: Date())
+                            viewModel.lastSync = viewModel.dateFormatter.string(from: Date())
                         }
                         .padding(.bottom)
                     VStack(spacing: 20) {
                         VStack(alignment: .leading) {
-                            TextField("Email", text: $email)
+                            TextField("Email", text: $viewModel.email)
                                 .padding()
                                 .background(Color(.secondarySystemBackground))
                                 .cornerRadius(10)
@@ -65,31 +31,13 @@ struct LoginView: View {
                                 .disableAutocorrection(true)
                         }
                         VStack(alignment: .leading) {
-                            SecureField("Password", text: $password)
+                            SecureField("Password", text: $viewModel.password)
                                 .padding()
                                 .background(Color(.secondarySystemBackground))
                                 .cornerRadius(10)
                         }
                         Button("Login", action: {
-                            isLoginInProgress = true
-                            Auth.auth().signIn(withEmail: self.email, password: self.password) { authResult, error in
-                                isLoginInProgress = false
-                                if let error = error {
-                                    print("Error logging in user: \(error.localizedDescription)")
-                                    loginError = error.localizedDescription
-                                    showLoginError.toggle()
-                                    return
-                                }
-                                fetchSocietyID(forUser: Auth.auth().currentUser!.uid) { (result) in
-                                    switch result {
-                                    case .success(let societyID):
-                                        print("User signed in successfully")
-                                        print("SocietyID: \(societyID)")
-                                    case .failure(let error):
-                                        print("Error fetching societyID: \(error)")
-                                    }
-                                }
-                            }
+                            viewModel.login()
                         })
                         .font(.largeTitle)
                         .buttonStyle(.bordered)
@@ -99,29 +47,25 @@ struct LoginView: View {
                         Text("Please do not share your login with anybody. You can request an additional admin account by emailing me or create volunteer accounts from within the app.")
                             .multilineTextAlignment(.center)
                         HStack {
-                            if let url = URL(string: newShelterForm) {
+                            if let url = URL(string: viewModel.newShelterForm) {
                                 Button(action: {
-                                    showNewShelterForm = true
+                                    viewModel.showNewShelterForm = true
                                 }) {
                                     Text("Create New Shelter")
                                 }
-                                .sheet(isPresented: $showNewShelterForm) {
+                                .sheet(isPresented: $viewModel.showNewShelterForm) {
                                     SafariView(url: url)
                                 }
-                            } else {
-//                                Text("Invalid URL")
                             }
-                            if let url = URL(string: tutorialsURL) {
+                            if let url = URL(string: viewModel.tutorialsURL) {
                                 Button(action: {
-                                    showTutorials = true
+                                    viewModel.showTutorials = true
                                 }) {
                                     Text("Tutorials/Documentation")
                                 }
-                                .sheet(isPresented: $showTutorials) {
+                                .sheet(isPresented: $viewModel.showTutorials) {
                                     SafariView(url: url)
                                 }
-                            } else {
-//                                Text("Invalid URL")
                             }
                         }
                         .buttonStyle(.bordered)
@@ -137,51 +81,17 @@ struct LoginView: View {
             .background(Color(.systemBackground))
         }
         .edgesIgnoringSafeArea(.bottom)
-        .toast(isPresenting: $showLoginError, duration: 3) {
+        .toast(isPresenting: $viewModel.showLoginError, duration: 3) {
             AlertToast(type: .error(.red), title: "Your username or password is incorrect.")
         }
-        .toast(isPresenting: $isLoginInProgress) {
+        .toast(isPresenting: $viewModel.isLoginInProgress) {
             AlertToast(type: .loading)
         }
         .onAppear {
-            fetchSignUpForm()
+            viewModel.fetchSignUpForm()
         }
         .onDisappear {
-            loginListener?.remove()
-        }
-    }
-
-    func fetchSocietyID(forUser userID: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let db = Firestore.firestore()
-
-        db.collection("Users").document(userID).getDocument { (document, error) in
-            if let error = error {
-                completion(.failure(error))
-            } else {
-                if let document = document, document.exists, let data = document.data(),
-                   let societyID = data["societyID"] as? String {
-                    completion(.success(societyID))
-                } else {
-                    completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "SocietyID not found"])))
-                }
-            }
-        }
-    }
-
-    func fetchSignUpForm() {
-        Firestore.firestore().collection("Stats").document("AppInformation").addSnapshotListener { (documentSnapshot, error) in
-            guard let document = documentSnapshot else {
-                print("Error fetching document: \(error!)")
-                return
-            }
-            guard let data = document.data() else {
-                print("Document data was empty.")
-                return
-            }
-            DispatchQueue.main.async {
-                self.newShelterForm = data["signUpForm"] as? String ?? ""
-                self.tutorialsURL = data["tutorialsURL"] as? String ?? ""
-            }
+            viewModel.loginListener?.remove()
         }
     }
 }
