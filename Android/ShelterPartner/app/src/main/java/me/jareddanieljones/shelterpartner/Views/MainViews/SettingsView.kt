170737c09756d7591b934476f72ef69def3e598e
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-09-30 17:49:38 -0500

    removed Android

--- Android/ShelterPartner/app/src/main/java/me/jareddanieljones/shelterpartner/Views/MainViews/SettingsView.kt
+++
@@ -1,9 +0,0 @@
-package me.jareddanieljones.shelterpartner.Views.MainViews
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SettingsView() {
-    Text("This is the settings view")
-}
