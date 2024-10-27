498fdb56aac3da324736f7ef8169349ddc5935dc
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-10-27 13:48:20 -0500

    257 implement photo uploads allowed setting (#275)
    * functionally working
    
    * Update add_note_view.dart

--- Flutter/shelter_partner/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/shelter_partner/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import cloud_firestore
 import cloud_functions
+import file_selector_macos
 import firebase_auth
 import firebase_core
+import firebase_storage
 import geolocator_apple
 import path_provider_foundation
 import sqflite_darwin
@@ -16,8 +18,10 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
