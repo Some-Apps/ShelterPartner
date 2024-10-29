30578e6525edf61d9026fb57d741026c7e470672
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-10-28 23:30:30 -0500

    260 implement custom form url and corresponding settings (#279)
    * Update put_back_confirmation_view.dart
    
    * working
    
    * Update put_back_confirmation_view.dart

--- Flutter/shelter_partner/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/shelter_partner/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,8 @@ import firebase_storage
 import geolocator_apple
 import path_provider_foundation
 import sqflite_darwin
+import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -25,4 +27,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
