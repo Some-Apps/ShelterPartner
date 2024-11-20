4ac35736d148bb8b5f3a44383729d8c646f14f3a
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-11-19 21:29:58 -0600

    website

--- Website/.firebase/shelterpartner/hosting/flutter_service_worker.js
+++ Website/.firebase/shelterpartner/hosting/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "56044eb12ad508fd0c5308acbfc34f15",
+const RESOURCES = {"flutter_bootstrap.js": "ba9442dd444519ea3011d3410a8c122e",
 "version.json": "980547175e325fe622a3362b84d55b6a",
 "index.html": "cd58bd31a15f54e373113d25880b65a1",
 "/": "cd58bd31a15f54e373113d25880b65a1",
-"main.dart.js": "b736cd0b718fb5f0405050d4a83168ed",
+"main.dart.js": "874cdff739d6e0341fb10ed20412d5a2",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
--- Website/lib/pages/home_page.dart
+++ Website/lib/pages/home_page.dart
@@ -8,125 +8,141 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-  leading: IconButton(
-    icon: Image.asset('assets/logo.png'),
-    onPressed: () {
-      Navigator.pushNamed(context, '/');
-    },
-  ),
-  title: Row(
-    mainAxisAlignment: MainAxisAlignment.end,
-    children: [
-      TextButton(
-        onPressed: () {
-          launchUrl(Uri(
-            scheme: 'https',
-            host: 'wiki.shelterpartner.org',
-          ));
-        },
-        child: const Text(
-          'Wiki',
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      TextButton(
-        onPressed: () {
-          launchUrl(Uri(
-            scheme: 'https',
-            host: 'github.com',
-            path: '/ShelterPartner/ShelterPartner',
-          ));
-        },
-        child: const Text(
-          'GitHub',
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.0),
-        child: SponsorButton(),
-      ), // Use the custom SponsorButton here
-    ],
-  ),
-),
-
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 800),
-            child: const Padding(
-              padding: EdgeInsets.all(40.0),
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Image.asset('assets/logo.png'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/');
+            },
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text(
-                'Welcome to Shelter Partner',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    Tooltip(
+                        message: "Coming January 1, 2025\nWindows\nMac\niOS\nAndroid\nWeb",
+                      child: TextButton(
+                        onPressed: () {
+                          // Handle the button press if needed
+                        },
+                        child: const Text(
+                          'Download',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+              TextButton(
+                onPressed: () {
+                  launchUrl(Uri(
+                    scheme: 'https',
+                    host: 'wiki.shelterpartner.org',
+                  ));
+                },
+                child: const Text(
+                  'Wiki',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              SizedBox(height: 16),
-              Text(
-                'Shelter Partner is a free and open source volunteer facing shelter management app that allows volunteers to have their own accounts and records visits. It’s highly customizable and connects directly to your ShelterLuv or ShelterManager account. It can also connect to Better Impact to sync your volunteers.',
-                style: TextStyle(fontSize: 15),
+              TextButton(
+                onPressed: () {
+                  launchUrl(Uri(
+                    scheme: 'https',
+                    host: 'github.com',
+                    path: '/ShelterPartner/ShelterPartner',
+                  ));
+                },
+                child: const Text(
+                  'GitHub',
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
-              SizedBox(height: 36),
-              Text(
-                'Create Account',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'As I transition to Version 2, I have disabled the ability to create an account. If you would like to create an account, Version 2 will be released on January 1, 2025. If you have any questions, feel free to email me at jared@shelterpartner.org',
-                style: TextStyle(fontSize: 15),
-              ),
-              SizedBox(height: 36),
-              Text(
-                'Version 2',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Version 2 is a complete rewrite of the app. It will be available on all platforms including web, iOS, Android, Windows, and Mac and will be released on January 1, 2025. A demo version of the app will be available on December 1, 2024. This will allow you to test the full functionality of the new version while helping me refine it and work out any bugs before the full release. If you have any questions, feel free to email me at jared@shelterpartner.org',
-                style: TextStyle(fontSize: 15),
-              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: SponsorButton(),
+              ), // Use the custom SponsorButton here
             ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 800),
+              child: const Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Welcome to Shelter Partner',
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Shelter Partner is a free and open source volunteer facing shelter management app that allows volunteers to have their own accounts and records visits. It’s highly customizable and connects directly to your ShelterLuv or ShelterManager account. It can also connect to Better Impact to sync your volunteers.',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(height: 36),
+                    Text(
+                      'Create Account',
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'As I transition to Version 2, I have disabled the ability to create an account. If you would like to create an account, Version 2 will be released on January 1, 2025. If you have any questions, feel free to email me at jared@shelterpartner.org',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(height: 36),
+                    Text(
+                      'Version 2',
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Version 2 is a complete rewrite of the app. It will be available on all platforms including web, iOS, Android, Windows, and Mac and will be released on January 1, 2025. A demo version of the app will be available on December 1, 2024. This will allow you to test the full functionality of the new version while helping me refine it and work out any bugs before the full release. If you have any questions, feel free to email me at jared@shelterpartner.org',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-        );
-      }
-    }
-
-
+    );
+  }
+}
 
 class SponsorButton extends StatelessWidget {
   const SponsorButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-      // Register the iframe element
-      // ignore: undefined_prefixed_name
-      ui.platformViewRegistry.registerViewFactory(
-        'iframeElement',
-        (int viewId) => html.IFrameElement()
-          ..src = 'https://github.com/sponsors/ShelterPartner/button'
-          ..style.border = '0'
-          ..style.borderRadius = '6px'
-          ..width = '114'
-          ..height = '32',
-      );
+    // Register the iframe element
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      'iframeElement',
+      (int viewId) => html.IFrameElement()
+        ..src = 'https://github.com/sponsors/ShelterPartner/button'
+        ..style.border = '0'
+        ..style.borderRadius = '6px'
+        ..width = '114'
+        ..height = '32',
+    );
 
-      return const SizedBox(
-        width: 114,
-        height: 32,
-        child: HtmlElementView(
-          viewType: 'iframeElement',
-        ),
-      );
-
-}
+    return const SizedBox(
+      width: 114,
+      height: 32,
+      child: HtmlElementView(
+        viewType: 'iframeElement',
+      ),
+    );
+  }
 }
