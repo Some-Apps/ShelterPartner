2201ae308a060da70fc628a211b6f1e68b6ec72b
vedantvijay <vedantvijaysingh7980@gmail.com>
2025-05-11 11:01:23 +0530

    test added

--- Flutter/shelter_partner/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/shelter_partner/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -21,7 +21,7 @@ import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
