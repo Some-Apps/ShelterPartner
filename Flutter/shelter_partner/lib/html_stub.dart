0fcb9e51a221d2b41a28bb1c66f23cc67afde2de
ayush srivastava <ayushsrivastavamail@gmail.com>
2025-06-10 00:37:00 +0530

    feat: Added Let Out Type and Early Put Back Reasons to Filter Conditions

---
+++ Flutter/shelter_partner/lib/html_stub.dart
@@ -0,0 +1,17 @@
+/// Stub implementation for dart:html for non-web platforms.
+/// This file is used to avoid import errors on iOS and Android.
+
+class Window {
+  Navigator get navigator => Navigator();
+  Location get location => Location();
+}
+
+class Navigator {
+  String get userAgent => '';
+}
+
+class Location {
+  void assign(String url) {}
+}
+
+final Window window = Window();
